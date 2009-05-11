a3f32d8fb292420b4ae31da4d26ddf45fb0b0b66
Manfred Stienstra <manfred@fngtps.com>
2009-05-11 16:37:53 +0200

    Document the code in the test_helper.

--- Flush/test/test_helper.rb
+++ Flush/test/test_helper.rb
@@ -11,10 +11,18 @@ require 'rucola/test_case'
 
 require File.expand_path('../../config/boot', __FILE__)
 
+# We want to make sure we never call system on Kernel because we might break something
 module Kernel
   def system(*)
     raise "Please stub Kernel#system methods in your tests so you don't break your system accidentally"
   end
 end
 
+# Not all OS X constants get loaded unless you require cocoa
+require 'osx/cocoa'
+
+# Some OS X clases need a runloop to function properly, let's just run one in a Thread somewhere
+Thread.new { OSX.CFRunLoopRun }
+
+# Rucola normally sets NSApp properly, but doesn't do this in tests
 OSX::NSApp = OSX::NSApplication.sharedApplication
