5496467affd45fe7c3d51152ef38dcd152f4d6ad
Manfred Stienstra <manfred@fngtps.com>
2009-05-11 10:16:28 +0200

    Add example application to write tests for.

---
+++ Flush/misc/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  Flush
+//
+//  Created by YOUR NAME on 2009-05-11.
+//  Copyright (c) 2007 YOUR NAME. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <RubyCocoa/RBRuntime.h>
+
+int main(int argc, const char *argv[])
+{
+    return RBApplicationMain("rb_main.rb", argc, argv);
+}
